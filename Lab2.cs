307c030beb516749702ebd0a92a5013f8f0c3ad2
OlegBodio <bodiosik@gmail.com>
2022-09-29 15:47:17 +0300

    Друга лабораторна робота виконана

---
+++ Lab2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Lab2
+{
+    class Program
+    {
+        static double x = 1, y = 5, z = 4;
+        static Semaphore S1 = new Semaphore(0, 1);
+        static Semaphore S2 = new Semaphore(0, 1);
+        static Semaphore S3 = new Semaphore(0, 2);
+        static void Main(string[] args)
+        {
+
+            //First Branch
+            Thread T1 = new Thread(() =>
+            {
+                x *= 5; S1.Release();
+            });
+
+
+            Thread T3 = new Thread(() =>
+            {
+                S1.WaitOne();
+                x += 2; S3.Release();
+            });
+
+
+            //Second Branch
+            Thread T2 = new Thread(() =>
+            {
+                y += 2; S2.Release();
+            });
+
+            Thread T4 = new Thread(() =>
+            {
+                S2.WaitOne();
+                y -= 3; S3.Release();
+            });
+
+            Thread T5 = new Thread(() =>
+            {
+                S3.WaitOne();
+                y *= x;
+            });
+
+            Thread[] threads = { T1, T2, T3, T4, T5 };
+            foreach(Thread t in threads)
+            {
+                t.Start();
+            }
+            foreach (Thread t in threads)
+            {
+                t.Join();
+            }
+
+            Console.WriteLine(y);
+        }
+    }
+}
